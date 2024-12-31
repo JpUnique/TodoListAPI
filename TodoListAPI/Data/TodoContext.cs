b4c36f5d780a7c4f04f8ca444ed058aaeacfef1e
dell <dell@JPUNIQUE>
2024-12-31 23:22:13 +0100

    Add project files.

---
+++ TodoListAPI/Data/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TodoListAPI.Models;
+
+namespace TodoListAPI.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; } // Represents the TodoItems table
+    }
+}
---
+++ TodoListAPI/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListAPI.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; } // Unique identifier
+        public string? Title { get; set; } // The task description
+        public bool IsCompleted { get; set; } // Status of the task
+    }
+}
